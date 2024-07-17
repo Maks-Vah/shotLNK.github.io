6e0e63cf051cc997d5fcd092e4ffdd0d5134e508
Maks-Vah <maks291101@mail.ru>
2024-07-18 02:35:20 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const api = require('./api'); 
-const app = express();
-const port = 3000;
-app.use('/',api)
-app.listen(port, () => {
-console.log(`Сервер запущен на порту ${port}`);
-});
